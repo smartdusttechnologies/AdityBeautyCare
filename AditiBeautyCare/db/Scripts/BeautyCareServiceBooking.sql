9109f28232efac633c151a9de399857d6f3b9f58
Shreenidhi H D <58159026+ShreenidhiHD@users.noreply.github.com>
2022-02-10 20:43:57 +0530

    Some UI Updated ,script updated
    Changed heading name in Admin dashboard table  UI, updated script

--- AditiBeautyCare/db/Scripts/BeautyCareServiceBooking.sql
+++ AditiBeautyCare/db/Scripts/BeautyCareServiceBooking.sql
@@ -2,7 +2,7 @@ CREATE TABLE [dbo].[BeautyCareServiceBooking]
 (
 	[Id] BIGINT IDENTITY (1, 1) NOT NULL,
 	[UserName] [varchar](50) NOT NULL,
-	[UserEmail] [varchar](250) NOT NULL,
+	[UserEmail] [varchar](250),
 	[Date] [varchar](50) NOT NULL,
 	[From] [varchar](50) NOT NULL,
 	[To] [varchar](50) NOT NULL,
