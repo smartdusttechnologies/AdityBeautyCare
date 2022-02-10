5d02240364cd7e29a08aa4849fac336270283e5b
Shreenidhi H D <58159026+ShreenidhiHD@users.noreply.github.com>
2022-02-10 23:41:26 +0530

    updated script  BeautyCareServiceBooking
    updated script  BeautyCareServiceBooking

--- AditiBeautyCare/db/Scripts/BeautyCareServiceBooking.sql
+++ AditiBeautyCare/db/Scripts/BeautyCareServiceBooking.sql
@@ -3,7 +3,7 @@ CREATE TABLE [dbo].[BeautyCareServiceBooking]
 	[Id] BIGINT IDENTITY (1, 1) NOT NULL,
 	[UserName] [varchar](50) NOT NULL,
 	[UserEmail] [varchar](250),
-	[Date] [varchar](50) NOT NULL,
+	[Date] [datetime] NOT NULL,
 	[From] [varchar](50) NOT NULL,
 	[To] [varchar](50) NOT NULL,
 	[IsDeleted] [bit] CONSTRAINT [D_BeautyCareServiceBooking_IsDeleted] DEFAULT ((0)) NOT NULL,
